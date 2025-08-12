2ab688a2d96e5b035667b29a4a672199bb02cc3a
pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2025-08-11 21:41:54 -0500

    I made progress on the second project, where I implemented a function to clean up user input and thus improve the user experience. I implemented the activation of the new game button using removeAttribute, I also grouped the initial conditions into a function so that everything could be restarted more easily, and finally I created a restartGame function, which resets all the necessary values so that the game can be restarted.

--- Logica_programacion_2/Second_Project/app.js
+++ Logica_programacion_2/Second_Project/app.js
@@ -1,23 +1,32 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
-console.log(numeroSecreto);
+//Creo las variables en 0 para que asi la funcion condiciones iniciales las pueda usar y colocar los valores que realmente necesito
+let numeroSecreto = 0;
+let intentos = 0;
 
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log('Numero de intentos: ' + intentos);
-
 
     if(numeroDeUsuario === numeroSecreto){
+        //El usuario acerto el numero
         asignarTextoElemento('p', `Acertaste el numero en ${intentos} ${(intentos === 1) ? 'intento':'intentos'}! Efectivamente era ${numeroSecreto} `);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
+        //El usuario no acerto el numero
         if(numeroDeUsuario > numeroSecreto){
             asignarTextoElemento('p',`El numero secreto es MENOR a ${numeroDeUsuario}`);
         } else{
             asignarTextoElemento('p',`El numero secreto es MAYOR a ${numeroDeUsuario}`);
         }
         intentos++
+        limpiarCaja();
     }
     return;
+}
+
+function limpiarCaja(){
+    //document.getElementById('valorUsuario') es lo mismo que gracias al #: 
+    let valorCaja = document.querySelector('#valorUsuario');
+    valorCaja.value = '';
 }
 
 function asignarTextoElemento(elemento, texto){
@@ -26,8 +35,27 @@ function asignarTextoElemento(elemento, texto){
     return;  
 }
 
-asignarTextoElemento('h1', 'Juego del numero secreto');
-asignarTextoElemento('p', 'Indica un numero del 1 al 100');
+function condicionesIniciales(){
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    asignarTextoElemento('h1', 'Juego del numero secreto');
+    asignarTextoElemento('p', 'Indica un numero del 1 al 10');
+}
+
+function reiniciarJuego(){
+    //Limpiar la caja
+    limpiarCaja();
+
+    //Indicar mensaje de intervalo de numero de inicio
+    //Generar el numero aleatorio
+    //Inicializar el numero de intentos
+    condicionesIniciales();
+
+    //Deshabilitar el boton de nuevo juego
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+}
+
+condicionesIniciales();
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random() * 10) + 1;
